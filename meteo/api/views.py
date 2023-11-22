8066cd0500a68fb44b6ff4ba06822591d52f9ef5
MissisOl <missisololol@gmail.com>
2023-11-22 23:35:09 +0300

    Comments deleted

--- meteo/api/views.py
+++ meteo/api/views.py
@@ -13,40 +13,25 @@ class BmeViewSet(viewsets.ModelViewSet):
 
 
 class BmeLastViewSet(viewsets.ModelViewSet):
-    """
-    This ViewSet automatically provides `list`, `create`, `retrieve`, `update` and `destroy` actions.
-    """
     queryset = BmeData.objects.all().order_by('-id')[:1]
     serializer_class = BmeSerializer
 
 
 class BmeHistoryViewSet(viewsets.ModelViewSet):
-    """
-    This ViewSet automatically provides `list`, `create`, `retrieve`, `update` and `destroy` actions.
-    """
     queryset = BmeHistory.objects.all().order_by('-id')
     serializer_class = BmeHistorySerializer
 
 
 class DhtViewSet(viewsets.ModelViewSet):
-    """
-    This ViewSet automatically provides `list`, `create`, `retrieve`, `update` and `destroy` actions.
-    """
     queryset = DhtData.objects.all().order_by('-id')
     serializer_class = DhtDataSerializer
 
 
 class DhtLastViewSet(viewsets.ModelViewSet):
-    """
-    This ViewSet automatically provides `list`, `create`, `retrieve`, `update` and `destroy` actions.
-    """
     queryset = DhtData.objects.all().order_by('-id')[:1]
     serializer_class = DhtDataSerializer
 
 
 class DhtHistoryViewSet(viewsets.ModelViewSet):
-    """
-    This ViewSet automatically provides `list`, `create`, `retrieve`, `update` and `destroy` actions.
-    """
     queryset = DhtHistory.objects.all().order_by('-id')
     serializer_class = DhtHistorySerializer
