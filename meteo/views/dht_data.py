8e6f08aeff44f4d58a3f4ab3bb0ba605340d1c61
MissisOl <missisololol@gmail.com>
2023-11-26 23:09:56 +0300

    Data views updated

--- meteo/views/dht_data.py
+++ meteo/views/dht_data.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
 
 from meteo.models import DhtData
-
+from meteo.modules.sensor_data import dht_data_list
 
 class DhtDataView(ListView):
     template_name = "meteo/dht_data.html"
@@ -11,26 +11,5 @@ class DhtDataView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["data_list"] = [
-            { 
-                'box': [
-                    {
-                        'dataName': "Температура, &deg;C",
-                        'className': 'bx bxs-thermometer readings',
-                        'id': 'temperature',
-                    }, 
-                    {
-                        'dataName': "Влажность, %", 
-                        'className': 'bx bxs-droplet-half readings readings--two',
-                        'id': 'humidity',
-                    }, 
-                ]
-            },
-            {
-                'gauge': [
-                    'temperature',
-                    'humidity',
-                ]
-            }
-        ]
+        context["data_list"] = dht_data_list
         return context
