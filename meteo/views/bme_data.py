de252704dbe0089b68ee547d60b7fa975c6235e7
MissisOl <missisololol@gmail.com>
2023-11-26 22:36:39 +0300

    Last template updated

--- meteo/views/bme_data.py
+++ meteo/views/bme_data.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
 
-from meteo.models import BmeData
+from meteo.models import BmeData, DhtData
 
 
 class BmeDataView(ListView):
@@ -9,6 +9,38 @@ class BmeDataView(ListView):
     queryset = BmeData.objects.order_by("-id")
     context_object_name = "bme_data"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["data_list"] = [
+           { 
+                'box': [
+                    {
+                        'dataName': "Температура, &deg;C",
+                        'className': 'bx bxs-thermometer readings',
+                        'id': 'temperature',
+                    }, 
+                    {
+                        'dataName': "Влажность, %", 
+                        'className': 'bx bxs-droplet-half readings readings--two',
+                        'id': 'humidity',
+                    }, 
+                    {
+                        'dataName': "Давление, мм.рт.ст.", 
+                        'className': 'bx bxs-tachometer readings readings--three',
+                        'id': 'pressure',
+                    },
+                ]
+            },
+            {
+                'gauge': [
+                    'temperature',
+                    'humidity',
+                    'pressure',
+                ]
+            }
+        ]
+        return context
+    
         
 
 
--- meteo/views/dht_data.py
+++ meteo/views/dht_data.py
@@ -8,3 +8,29 @@ class DhtDataView(ListView):
     template_name = "meteo/dht_data.html"
     queryset = DhtData.objects.order_by("-id")[:10]
     context_object_name = "dht_data"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["data_list"] = [
+            { 
+                'box': [
+                    {
+                        'dataName': "Температура, &deg;C",
+                        'className': 'bx bxs-thermometer readings',
+                        'id': 'temperature',
+                    }, 
+                    {
+                        'dataName': "Влажность, %", 
+                        'className': 'bx bxs-droplet-half readings readings--two',
+                        'id': 'humidity',
+                    }, 
+                ]
+            },
+            {
+                'gauge': [
+                    'temperature',
+                    'humidity',
+                ]
+            }
+        ]
+        return context
