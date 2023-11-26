bcf7f5dfaee8f782446b14205590b5c9c497e620
MissisOl <missisololol@gmail.com>
2023-11-27 00:12:55 +0300

    Utils folder added

--- meteo/views/dht_data.py
+++ meteo/views/dht_data.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
 
 from meteo.models import DhtData
-from meteo.modules.sensor_data import dht_data_list
+from meteo.utils.sensor_data import data_list
 
 class DhtDataView(ListView):
     template_name = "meteo/dht_data.html"
@@ -11,5 +11,5 @@ class DhtDataView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["data_list"] = dht_data_list
+        context["data_list"] = data_list
         return context
