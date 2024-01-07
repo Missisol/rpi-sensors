ce992e02862e1087580f9165a76f1942a7aa6a8d
MissisOl <missisololol@gmail.com>
2024-01-07 23:46:01 +0300

    Date picker constraint updated

--- meteo/static/js/modules/helpers.js
+++ meteo/static/js/modules/helpers.js
@@ -1,15 +1,17 @@
-const minDate = '2023-11-08'
+const minDate = '2023-11-08';
+const yesterdayRatio = 1000 * 60 * 60 * 24;
 
-const getDate = () => {
+const getCurrentDate = () => {
   const currentDate = new Date();
-  const year = currentDate.getFullYear();
-  const day = currentDate.getDay() < 10 ? `0${currentDate.getDay()}` : currentDate.getDay();
-  const month = currentDate.getMonth() < 9 ? `0${currentDate.getMonth() + 1}` : currentDate.getMonth() + 1;
+  const yesterday = new Date(currentDate - yesterdayRatio);
+  const year = yesterday.getFullYear();
+  const day = yesterday.getDate() < 10 ? `0${yesterday.getDate()}` : yesterday.getDate();
+  const month = yesterday.getMonth() < 9 ? `0${yesterday.getMonth() + 1}` : yesterday.getMonth() + 1;
   return { year, month, day };
 }
 
 const getFormettedDate = () => {
-  const { year, month, day } = getDate();
+  const { year, month, day } = getCurrentDate();
   return `${year}-${month}-${day}`;
 }
 
