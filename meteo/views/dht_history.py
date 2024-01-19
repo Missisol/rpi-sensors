b9911e617e5d6394ef927bacba380b3271849174
MissisOl <missisololol@gmail.com>
2024-01-19 07:18:40 +0300

    For loop added

--- meteo/views/dht_history.py
+++ meteo/views/dht_history.py
@@ -29,33 +29,34 @@ class Dht2HistoryView(ListView):
 
 class DhtHistoryQuery():
     def get_minmax_dht_data(self, queryObj, queryHistoryObj, n):
-        yesterday =  date.today() - timedelta(days=1)
-        dht_yesterday_data = queryObj.objects.filter(date=yesterday)
-        history_yestarday_data = queryHistoryObj.objects.filter(date=yesterday)
-        
-        try:
-            if dht_yesterday_data.exists() and not history_yestarday_data.exists():
-                minmax = dht_yesterday_data.aggregate(
-                    Min('temperature'), 
-                    Max('temperature'), 
-                    Min('humidity'), 
-                    Max('humidity'), 
-                )
-                history = queryHistoryObj(
-                    date = yesterday,
-                    min_temperature = minmax['temperature__min'], 
-                    max_temperature = minmax['temperature__max'], 
-                    min_humidity = minmax['humidity__min'], 
-                    max_humidity = minmax['humidity__max'],  
-                )
-                history.save()
-                queryObj.objects.filter(date=yesterday).delete()
-                print(f'History-{n} saved and DHT yesterday data deleted')
-            elif dht_yesterday_data.exists() and history_yestarday_data.exists():
-                queryObj.objects.filter(date=yesterday).delete()
-                print(f'DHT-{n} yesterday data deleted')
-            else:
-                print(f'DHT-{n} data has already been deleted')
-        except Exception as e:
-                print(e)
+        for x in range(1, 10):
+            yesterday =  date.today() - timedelta(days=x)
+            dht_yesterday_data = queryObj.objects.filter(date=yesterday)
+            history_yestarday_data = queryHistoryObj.objects.filter(date=yesterday)
+            
+            try:
+                if dht_yesterday_data.exists() and not history_yestarday_data.exists():
+                    minmax = dht_yesterday_data.aggregate(
+                        Min('temperature'), 
+                        Max('temperature'), 
+                        Min('humidity'), 
+                        Max('humidity'), 
+                    )
+                    history = queryHistoryObj(
+                        date = yesterday,
+                        min_temperature = minmax['temperature__min'], 
+                        max_temperature = minmax['temperature__max'], 
+                        min_humidity = minmax['humidity__min'], 
+                        max_humidity = minmax['humidity__max'],  
+                    )
+                    history.save()
+                    queryObj.objects.filter(date=yesterday).delete()
+                    print(f'History-{n} saved and DHT yesterday data deleted')
+                elif dht_yesterday_data.exists() and history_yestarday_data.exists():
+                    queryObj.objects.filter(date=yesterday).delete()
+                    print(f'DHT-{n} yesterday data deleted')
+                else:
+                    print(f'DHT-{n} data has already been deleted')
+            except Exception as e:
+                    print(e)
 
